3b75fc6e32bf126da901c8ccb183077d989979ec
FrandelDev <frandel-c.r@outlook.com>
2024-04-01 09:17:49 -0400

    The Response Modeling Service has been completed

---
+++ src/CustomerOperationsApi/Models/ResponseApiModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerOperationsApi.Models
+{
+    public class ResponseApiModel
+    {
+        public int StatusCode { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+
+        public dynamic? Data { get; set; }
+
+
+    }
+}
---
+++ src/CustomerOperationsApi/Services/ResponseApiService.cs
@@ -0,0 +1,31 @@
+﻿using CustomerOperationsApi.Models;
+
+namespace CustomerOperationsApi.Services
+{
+    public static class ResponseApiService
+    {
+
+        public static ResponseApiModel Response(int statusCode, string message=null, object data=null)
+        {
+           
+            if (statusCode >= 200 && statusCode <= 300)
+            {
+                return new ResponseApiModel()
+                {
+                    StatusCode = statusCode,
+                    Success = true,
+                    Message = message,
+                    Data = data
+                };
+            }
+            return new ResponseApiModel()
+            {
+                StatusCode = statusCode,
+                Success = false,
+                Message = message,
+                Data = data
+            }; ;
+
+        }
+    }
+}
