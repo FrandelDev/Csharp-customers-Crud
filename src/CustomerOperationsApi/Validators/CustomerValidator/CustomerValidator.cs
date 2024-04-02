f511d378fda0f229bf0757e6c78e545acc8d1e00
FrandelDev <frandel-c.r@outlook.com>
2024-04-02 06:37:41 -0400

    Validations added

---
+++ src/CustomerOperationsApi/Validators/CustomerValidator/CustomerValidator.cs
@@ -0,0 +1,35 @@
+﻿using CustomerOperationsApi.Models;
+using System.Text.RegularExpressions;
+
+namespace CustomerOperationsApi.Validators.CustomerValidator
+{
+    public static class CustomerValidator
+    {
+
+        public static ValidationResponse IsValid(string id)
+        {
+          
+            if (id.Length != 13 || !Regex.IsMatch(id, "\\d{3}-\\d{7}-\\d"))
+            {
+                return new ValidationResponse(false,"Incorrect identificator format");
+            }
+           
+            return new ValidationResponse(true);
+        }
+    }
+
+
+     
+
+    public class ValidationResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+
+        public ValidationResponse(bool success, string? message=null)
+        {
+            this.IsSuccess = success;
+            this.Message = message;
+        }
+    }
+}
