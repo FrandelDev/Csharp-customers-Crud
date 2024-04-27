628c31172cf92a5a9c66d5bcf74b381fc36f8a8c
FrandelDev <frandel-c.r@outlook.com>
2024-04-27 10:34:01 -0400

    get one customer

--- src/CustomerOperations-Frontend/scripts/GetOneCustomer.js
+++ src/CustomerOperations-Frontend/scripts/GetOneCustomer.js
@@ -1,10 +1,18 @@
 import { clear } from "./clear.js";
 
 const btns = document.querySelector("#btns");
-const btnGetOne = document.querySelector("#get-one")
+const btnGetOne = document.querySelector("#get-one");
+const url = "http://localhost:5001/api/Customer/";
 
-async function GetOneCustomer(){
-    
+function GetOneCustomer(){
+   document.querySelector("#btn-search").addEventListener('click', async(event)=>{
+        event.preventDefault();
+        const res = await fetch(url+ document.querySelector("#IdCardNumberInputGenerated").value);
+        const data = await res.json()
+     
+        console.log(data);
+    });
+
     
 }
 
@@ -16,7 +24,9 @@ function renderInput(){
     <label for="IdCardNumberInputGenerated" id="IdCardNumberGenerated">
     Identification Card Number:
     <input type="text" id="IdCardNumberInputGenerated" placeholder="000-0000000-0">
+    <button id="btn-search">Search</button>
     </label>
     `);
+    GetOneCustomer()
 }
-export {GetOneCustomer}
+export {renderInput,GetOneCustomer}
